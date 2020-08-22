fd38a721cc81291942c13ddd24fed987a763f7f7
Garo San <garosanchez27@gmail.com>
2020-08-22 00:28:37 -0500

    Add sass, basic homepage structure and homepage styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./homepage.component";
 
-function App() {
-  return <div>Hello Crwn Clothing Project!</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+};
 
 export default App;
