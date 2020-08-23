8c113f259c36933ce3d6eac8caa5ea9e2d77784c
Garo San <garosanchez27@gmail.com>
2020-08-23 02:10:52 -0500

    Create SignIn Page, FormInput, SignIn components and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 const App = () => {
@@ -11,7 +12,8 @@ const App = () => {
       <Header></Header>
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/shop" exact component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
