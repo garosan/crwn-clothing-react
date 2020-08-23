0407f9cf734dc05b47aa130e7728158b2cd8c8ae
Garo San <garosanchez27@gmail.com>
2020-08-22 20:59:41 -0500

    Added shop page, collection item and collection preview

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const HatsCompletedPage = () => (
-  <div>
-    <h1>HATS COMPLETED PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 const App = () => {
   return (
     <div>
       <Route path="/" exact component={HomePage} />
-      <Route path="/hats" exact component={HatsPage} />
-      <Route path="/hats/completed" component={HatsCompletedPage} />
+      <Route path="/shop" exact component={ShopPage} />
     </div>
   );
 };
