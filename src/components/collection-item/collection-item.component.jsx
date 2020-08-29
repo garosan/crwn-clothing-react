a4c43ad13799a4a78833fdc3bb52400d229b64ad
Garo San <garosanchez27@gmail.com>
2020-08-29 14:07:11 -0500

    Add and style "Add to cart button" to collection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
+import CustomButton from "../custom-button/custom-button.component";
+
 import "./collection-item.styles.scss";
-import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
@@ -11,6 +12,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
